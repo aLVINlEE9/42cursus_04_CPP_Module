700a9901436167da7e3cd09985cd4dcd086f3255
aLVINlEE9 <fldh3746@gmail.com>
2022-07-25 15:53:43 +0900

    cpp00/ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else if (argc >= 2) {
+		for (int i = 1; i < argc; ++i)
+		{
+			for (int j = 0; j < (int)strlen(argv[i]); ++j)
+			{
+				putchar(toupper(argv[i][j]));
+			}
+		}
+		std::cout << std::endl;
+	}
+}
