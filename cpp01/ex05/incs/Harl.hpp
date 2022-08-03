b08f04cba69c9323aaa279016485d77ed5d54c45
aLVINlEE9 <fldh3746@gmail.com>
2022-08-03 17:27:21 +0900

    cpp01/ex05 done

---
+++ cpp01/ex05/incs/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 16:57:54 by seungsle          #+#    #+#             */
+/*   Updated: 2022/08/03 17:01:26 by seungsle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_CLASS_H
+# define HARL_CLASS_H
+
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Harl();
+		~Harl();
+		void complain( std::string level );
+};
+
+#endif
---
+++ cpp01/ex05/srcs/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/03 16:57:33 by seungsle          #+#    #+#             */
+/*   Updated: 2022/08/03 17:25:56 by seungsle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return (0);
+}
