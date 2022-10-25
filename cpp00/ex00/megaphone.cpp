73306bd8f656812bedc5499809c561ced621d271
aLVINlEE9 <fldh3746@gmail.com>
2022-10-25 16:55:52 +0900

    cpp00/ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,27 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seungsle <seungsle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: seungsle <seungsle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/25 19:14:24 by seungsle          #+#    #+#             */
-/*   Updated: 2022/07/25 19:14:25 by seungsle         ###   ########.fr       */
+/*   Created: 2022/10/25 12:23:11 by seungsle          #+#    #+#             */
+/*   Updated: 2022/10/25 12:36:53 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv) {
-	if (argc == 1) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
-	else if (argc >= 2) {
-		for (int i = 1; i < argc; ++i)
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" ;
+	else
+	{
+		for (int i = 1; i < argc; i++)
 		{
-			for (int j = 0; j < (int)strlen(argv[i]); ++j)
+			for (int j = 0; j < (int)strlen(argv[i]); j++)
 			{
-				putchar(toupper(argv[i][j]));
+				std::cout << (char)toupper(argv[i][j]);
 			}
 		}
-		std::cout << std::endl;
+		std::cout << "\n";
 	}
+	return (0);
 }
